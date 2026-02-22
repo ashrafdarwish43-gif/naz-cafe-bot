9e1e4df91287b390e5a69c404bc9ee24109d5d74
ashrafdarwish43-gif <ashrafdarwish43@gmail.com>
2026-02-22 02:00:53 +0200

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  userId: String,
+  guildId: String,
+  xp: { type: Number, default: 0 },
+  level: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model("User", userSchema);
