49dc2c0705fd244391fd409d6087eb747a7fc78a
ashrafdarwish43-gif <ashrafdarwish43@gmail.com>
2026-02-23 02:13:13 +0200

    Create server.js

---
+++ dashboard/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    botName: "NAZ Cafe",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🌐 Dashboard running on port " + PORT);
+});
