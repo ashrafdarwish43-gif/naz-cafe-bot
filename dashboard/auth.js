769579fffc7d4c6d19e6e8287d36180163612ef9
ashrafdarwish43-gif <ashrafdarwish43@gmail.com>
2026-03-04 21:01:31 +0200

    Create auth.js

---
+++ dashboard/auth.js
@@ -0,0 +1,21 @@
+const passport = require("passport");
+const DiscordStrategy = require("passport-discord").Strategy;
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
+
+passport.use(
+  new DiscordStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+      scope: ["identify", "guilds"]
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+module.exports = passport;
